be4a87222d6aedbead509a8a9342c5d8f68c87b0
SkDragonflay <99594424+SkDragonflay@users.noreply.github.com>
2022-11-21 01:02:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,19 +34,36 @@ Bot = Client(name="auto-delete",
 
 
 @Bot.on_message(filters.command('start') & filters.private)
-async def start(bot, message):
-    await message.reply(START_MSG.format(message.from_user.mention))
+async def clean_data():
+    print("checking media")
+    for ids in ub.search_messages(chat_id=group, filter="photo_video", limit=20):
+        msg_id = ids.message_id
+        idss.append(msg_id)
+        ub.copy_message(chat_id=channel, from_chat_id=group, message_id=msg_id)
+        ub.delete_messages(chat_id=group, message_ids=msg_id)
+    else:
+        if len(idss) == 0:
+            print("no photos to delete")
+            return
+        else:
+            c = len(idss)
+            print(f"cleared almost {c} messages")
+            idss.clear() 
 
-@User.on_message(filters.chat(GROUPS))
-async def delete(user, message):
-    try:
-       if message.from_user.id in ADMINS:
-          return
-       else:
-          await asyncio.sleep(TIME)
-          await Bot.delete_messages(message.chat.id, message.id)
-    except Exception as e:
-       print(e)
+    for ids in ub.search_messages(chat_id=group, filter="document", limit=5):
+        msg_id = ids.message_id
+        idss.append(msg_id)
+        ub.copy_message(chat_id=channel, from_chat_id=group, message_id=msg_id)
+        ub.delete_messages(chat_id=group, message_ids=msg_id)
+    else:
+        if len(idss) == 0:
+            print("no photos to delete")
+            return
+        else:
+            c = len(idss)
+            print(f"almost {c} files deleted")
+            idss.clear()     
+
        
 User.start()
 print("User Started!")
